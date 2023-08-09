635ea0adb01b83de331513a78adabec473fdfe59
Vinicius Lima <vinicius.lima.brasil1@gmail.com>
2023-08-09 16:28:32 -0300

    feat: special movie - castling

--- src/chess/pieces/King.java
+++ src/chess/pieces/King.java
@@ -2,14 +2,17 @@ package chess.pieces;
 
 import boardgame.Board;
 import boardgame.Position;
+import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.Color;
 
 public class King extends ChessPiece {
+	
+	private ChessMatch chessMatch;
 
-	public King(Board board, Color color) {
+	public King(Board board, Color color, ChessMatch chessMatch) {
 		super(board, color);
-
+		this.chessMatch = chessMatch;
 	}
 
 	@Override
@@ -69,6 +72,36 @@ public class King extends ChessPiece {
 		if (getBoard().positionExists(p) && canMove(p)) {
 			mat[p.getRow()][p.getColumn()] = true;
 		}
+		
+		// special move castling
+		if(getMoveCount() == 0 && !chessMatch.getCheck()) {
+			// castling kingside
+			Position kingSideRookPosition = new Position(position.getRow(), position.getColumn() + 3);
+			
+			if(testRookCastling(kingSideRookPosition)) {
+				Position firstPositionBetweenRook = new Position(position.getRow(), position.getColumn() + 1);
+				Position secondPositionBetweenRook = new Position(position.getRow(), position.getColumn() + 2);
+				
+				if(getBoard().piece(firstPositionBetweenRook) == null && getBoard().piece(secondPositionBetweenRook) == null) {
+					mat[position.getRow()][position.getColumn() + 2] = true;
+				}
+				
+			}
+			
+			// castling queenside
+			Position queenSideRookPosition = new Position(position.getRow(), position.getColumn() - 4);
+			
+			if(testRookCastling(queenSideRookPosition)) {
+				Position firstPositionBetweenRook = new Position(position.getRow(), position.getColumn() - 1);
+				Position secondPositionBetweenRook = new Position(position.getRow(), position.getColumn() - 2);
+				Position thirdPositionBetweenRook = new Position(position.getRow(), position.getColumn() - 3);
+				
+				if(getBoard().piece(firstPositionBetweenRook) == null && getBoard().piece(secondPositionBetweenRook) == null && getBoard().piece(thirdPositionBetweenRook) == null) {
+					mat[position.getRow()][position.getColumn() - 2] = true;
+				}
+				
+			}
+		}
 
 		return mat;
 	}
@@ -76,6 +109,12 @@ public class King extends ChessPiece {
 	private boolean canMove(Position position) {
 		ChessPiece p = (ChessPiece) getBoard().piece(position);
 		return p == null || p.getColor() != getColor();
+	}
+	
+	private boolean testRookCastling(Position position) {
+		ChessPiece p = (ChessPiece)getBoard().piece(position);
+		
+		return p != null && p instanceof Rook && p.getColor() == getColor() && p.getMoveCount() == 0;
 	}
 
 }
