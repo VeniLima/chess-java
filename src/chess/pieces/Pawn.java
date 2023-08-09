5ea814343f81c56ae552e796c8893009f65bb12f
Vinicius Lima <vinicius.lima.brasil1@gmail.com>
2023-08-09 03:38:57 -0300

    feat: class Pawn added to the game

---
+++ src/chess/pieces/Pawn.java
@@ -0,0 +1,73 @@
+package chess.pieces;
+
+import boardgame.Board;
+import boardgame.Position;
+import chess.ChessPiece;
+import chess.Color;
+
+public class Pawn extends ChessPiece {
+
+	public Pawn(Board board, Color color) {
+		super(board, color);
+	}
+
+	@Override
+	public String toString() {
+		return "P";
+	}
+
+	@Override
+	public boolean[][] possibleMoves() {
+		boolean[][] possibleMovesMatrix = new boolean[getBoard().getRows()][getBoard().getColumns()];
+		Position p = new Position(0, 0);
+		if (getColor() == Color.WHITE) {
+			p.setValues(position.getRow() - 1, position.getColumn());
+			if (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+				possibleMovesMatrix[p.getRow()][p.getColumn()] = true;
+			}
+
+			p.setValues(position.getRow() - 2, position.getColumn());
+			Position aux = new Position(position.getRow() - 1, position.getColumn());
+			if (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p) && getBoard().positionExists(aux)
+					&& !getBoard().thereIsAPiece(aux) && getMoveCount() == 0) {
+				possibleMovesMatrix[p.getRow()][p.getColumn()] = true;
+			}
+
+			p.setValues(position.getRow() - 1, position.getColumn() - 1);
+			if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+				possibleMovesMatrix[p.getRow()][p.getColumn()] = true;
+			}
+
+			p.setValues(position.getRow() - 1, position.getColumn() + 1);
+			if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+				possibleMovesMatrix[p.getRow()][p.getColumn()] = true;
+			}
+
+		} else {
+			p.setValues(position.getRow() + 1, position.getColumn());
+			if (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+				possibleMovesMatrix[p.getRow()][p.getColumn()] = true;
+			}
+
+			p.setValues(position.getRow() + 2, position.getColumn());
+			Position aux = new Position(position.getRow() + 1, position.getColumn());
+			if (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p) && getBoard().positionExists(aux)
+					&& !getBoard().thereIsAPiece(aux) && getMoveCount() == 0) {
+				possibleMovesMatrix[p.getRow()][p.getColumn()] = true;
+			}
+
+			p.setValues(position.getRow() + 1, position.getColumn() - 1);
+			if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+				possibleMovesMatrix[p.getRow()][p.getColumn()] = true;
+			}
+
+			p.setValues(position.getRow() + 1, position.getColumn() + 1);
+			if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+				possibleMovesMatrix[p.getRow()][p.getColumn()] = true;
+			}
+		}
+
+		return possibleMovesMatrix;
+	}
+
+}
